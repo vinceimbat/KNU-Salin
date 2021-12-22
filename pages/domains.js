d65f706d6b9120e079b3bab95ba11c2d7181ff67
Joe Sycalik <sycalik@gmail.com>
2021-12-22 13:04:32 -0500

    Add page: custom domain setup article

---
+++ pages/domains.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>My Digital Garden!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <body>
+        <iframe src="https://www.craft.do/s/eJP7YaTF7l1M2T"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            width="100%"
+            height="100%"
+            scrolling="auto">
+        </iframe>
+      </body>
+    </div>
+  )
+}
