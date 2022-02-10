e43f4f97d43cf3e06546017f0476f1eb3a9c7f1f
vinceimbat <96913392+vinceimbat@users.noreply.github.com>
2022-02-10 20:19:25 +0800

    Replace iframe src with knu salin homepage link

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <body>
-        <iframe src="https://www.craft.do/s/NIXWQEoYecjzBt"
+        <iframe src="https://www.craft.do/s/LXnNJVva6enIph"
             frameborder="0"
             marginheight="0"
             marginwidth="0"
